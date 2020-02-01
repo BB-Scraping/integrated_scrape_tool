ba6d2e86dac028115ed7e0d233488b261095dda6
Vishva Saravanan <vishva2912@gmail.com>
2020-02-02 00:41:58 +0530

    Purged venv

--- BB_spider/bb/bb/spiders/bb_spider.py
+++ BB_spider/bb/bb/spiders/bb_spider.py
@@ -2,6 +2,10 @@ import scrapy
 
 site_idx = 0
 
+def humanize(data):
+	humanized_data = string[2:-2]
+	return humanized_data
+
 class bb_spider(scrapy.Spider):
 	name = 'bb_spider'
 	f = open('toscrape.txt', 'r')
@@ -10,7 +14,7 @@ class bb_spider(scrapy.Spider):
 		global site_idx
 		page = response.url.split('/')[-1]
 		data = str(response.css('#ctl00_MainContentPlaceholder_C006_forumsFrontendPostsList_ctl00_ctl00_list_ctrl0_ContentHtml *::text').getall())
-		# data = humanize(data)
+		data = humanize(data)
 		data = data.encode()
 		filename = '%d.txt' % site_idx
 		site_idx += 1
