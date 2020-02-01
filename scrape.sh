c165f55f58f219733700958c2eb172e733e956bf
Vishva Saravanan <vishva2912@gmail.com>
2020-02-02 01:37:52 +0530

    Generate raw links file for approx 1000 articles

--- scrape.sh
+++ scrape.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+for((i=2;i<23;i++))
+do
+	echo https://www.beyondblue.org.au/get-support/online-forums/depression/page/"$i" >> raw.txt
+done
+mv raw.txt URL_spider/url
 cd URL_spider/url
 python3 -m scrapy crawl url_spider
 mv toscrape.txt ../../BB_spider/bb/
 cd ../../BB_spider/bb
 python3 -m scrapy crawl bb_spider
+mv output.xls ../../
